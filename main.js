dbdf7e86acc1a4bdd3ccb73a0b882aff6c575859
Harvard-is-cool <100159216+Harvard-is-cool@users.noreply.github.com>
2022-02-21 13:48:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function ImageSearch() {
+    window.location = "Google Image Search.html";
+}
+
+function AdvancedSearch() {
+    window.location = "Google Advanced Search.html";
+}
+
+function Back() {
+    window.location = "Google Search.html";
+}
+
+function lucky_function() {
+    random = Math.floor(Math.random() * 11) + 1;
+    if (random == 1) {
+        window.open("https://www.google.com/search?q=I%27m+Feeling+Curious&csf=b&ct=ifl&cad=2:curious&ei=t8cTYsm7MqykptQPmLuM0As&ved=0ahUKEwiJ89KRpZH2AhUskokEHZgdA7oQnRsIEA&rct=j");
+    } else if (random == 2) {
+        window.open("https://www.google.com/search?q=dinner+recipes&ct=ifl&cad=2:hungry&ei=qscTYvXGAZieptQPs-a7mAc&ved=0ahUKEwj1w4iLpZH2AhUYj4kEHTPzDnMQnRsIEA&rct=j");
+    } else if (random == 3) {
+        window.open("https://www.google.com/search?q=roll+a+die&csf=b&ct=ifl&cad=2:adventurous&ei=rscTYpCiCPGlptQP0IOwqAY&ved=0ahUKEwiQsYONpZH2AhXxkokEHdABDGUQnRsIEA&rct=j");
+    } else if (random == 4) {
+        window.open("http://www.agoogleaday.com/#date=2011-06-04");
+    } else if (random == 5) {
+        window.open("https://www.google.com/search?q=tic+tac+toe&csf=b&ct=ifl&cad=2:playful&ei=sMcTYv3SLZWeptQPjJmS-AE&ved=0ahUKEwj96qKOpZH2AhUVj4kEHYyMBB8QnRsIEA&rct=j");
+    } else if (random == 6) {
+        window.open("https://www.google.com/search?q=nebulae&um=1&ie=UTF-8&tbm=isch&sxsrf=APq-WBtKiEJ6KmSOBop3366OOf6s-2uxag:1645463474485&csf=b");
+    } else if (random == 7) {
+        window.open("https://donate.google.com/checkout?campaignid=6420545008435200");
+    } else if (random == 8) {
+        window.open("https://www.google.com/search?q=google+doodles&csf=b&ct=ifl&cad=2:doodley&ei=tMcTYqLsJ4qhptQPl82HuAE&ved=0ahUKEwiilpGQpZH2AhWKkIkEHZfmARcQnRsIEA&rct=j");
+    } else if (random == 9) {
+        window.open("https://trends.google.com/trends/trendingsearches/daily?geo=US");
+    } else if (random == 10) {
+        window.open("https://artsandculture.google.com/asset/twGyqq52R-lYpA");
+    } else if (random == 11) {
+        window.open("https://www.google.com/search?q=funny+cat+memes&um=1&ie=UTF-8&tbm=isch&sxsrf=APq-WBuJsTWeYi3NjrFfZC75PX0onRE9Rw:1645463478802&csf=b");
+    }
+}
